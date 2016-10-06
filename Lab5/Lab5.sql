a05da6c3a03ccbeeb88057c53de289fb3046060d
Brian Henderson <brian.henderson1@marist.edu>
2016-10-05 20:39:32 -0400

    Tested, Tested, Tested, Cleaned up some spacing and other formating keeping in mind the most important principle W.W.A.D (What Would Alan Do), Tested some more - no errors. Done.

--- Lab5/Lab5.sql
+++ Lab5/Lab5.sql
@@ -13,7 +13,7 @@ WHERE o.aid = a.aid
   
 -- 2: Shows the ids of products ordered through any agent who makes at least one order for
 --    a customer in Kyoto, sorted by PID from highest to lowest. --
-SELECT distinct ord2.pid
+SELECT DISTINCT ord2.pid
 FROM customers c,
      orders ord1 FULL OUTER JOIN orders ord2 ON ord1.aid = ord2.aid
 WHERE c.city = 'Kyoto'
@@ -36,7 +36,9 @@ WHERE o.cid is null ;
 -- 5: Shows the names of customers who place at least one order through an agent in their 
 --    own city along with those agents names --
 SELECT DISTINCT c.name AS "CustomerName" , a.name AS "AgentName"
-FROM customers c, agents a, orders o
+FROM customers c, 
+     agents a, 
+     orders o
 WHERE o.cid = c.cid 
   AND o.aid = a.aid
   AND c.city = a.city ;
@@ -52,7 +54,7 @@ FROM customers
 WHERE city IN ( SELECT city
                 FROM products
                 GROUP BY city
-                ORDER BY count(*) ASC 
+                ORDER BY COUNT(*) ASC 
                 LIMIT 1
               )
 ;
